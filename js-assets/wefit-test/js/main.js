11c31192347d0062d9a6a05d3286bb32cbd4d857
Daniel Toloto <dctoloto@gmail.com>
2024-03-12 21:15:40 -0300

    add header styling

---
+++ js-assets/wefit-test/js/main.js
@@ -0,0 +1,9 @@
+// STYLING HEADER
+const jumbotron = document.querySelector('#jumbotron');
+jumbotron.classList.add('text-right');
+jumbotron.classList.add('bg-secondary');
+jumbotron.classList.add('text-white');
+
+const jumbotronBtn = document.querySelector('#jumbotron-btn');
+jumbotronBtn.classList.remove('btn-primary');
+jumbotronBtn.classList.add('btn-success');
